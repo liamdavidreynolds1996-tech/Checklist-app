c6d1ec26187b46d4906692d5a1b0330c145c58b6
Liam <liamr@user.local>
2025-12-30 16:21:20 +0000

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/checklist-app/',
   plugins: [
     react(),
     VitePWA({
